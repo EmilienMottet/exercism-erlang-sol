970d33005ac0d2c6d438976e1a926fbd116c9c22
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-11-16 17:47:09 +0100

    fix sum-of-multiples

--- sum-of-multiples/src/sum_of_multiples.erl
+++ sum-of-multiples/src/sum_of_multiples.erl
@@ -4,8 +4,10 @@
 
 sum(Factors, Limit) ->
     lists:sum(
-      lists:usort(
-        lists:flatmap(
-          fun (X) -> lists:seq(0,Limit,X) end, Factors)
-       )
-     ).
+        lists:usort(
+            lists:flatmap(fun (X) when X > 0 ->
+                                  lists:seq(0, Limit - 1, X);
+                              (_X) ->
+                                  []
+                          end,
+                          Factors))).
